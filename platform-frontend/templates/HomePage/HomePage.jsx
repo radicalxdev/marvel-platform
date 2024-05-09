aa969620fdd2c82793e9ceb56c92f341e45772f7
Billy K. Bett <82142770+bkb-Git@users.noreply.github.com>
2024-05-09 13:28:38 +0300

    added all tools category

--- platform-frontend/templates/HomePage/HomePage.jsx
+++ platform-frontend/templates/HomePage/HomePage.jsx
@@ -2,6 +2,27 @@ import { Grid, Typography } from '@mui/material';
 
 import styles from './styles';
 
+const DEFAULT_TOOLS = [
+  {
+    id: 1,
+    name: 'Kai Tools',
+    description:
+      'Kai Tools is a collection of AI tools that can be used to help you with your AI learning journey.',
+  },
+  {
+    id: 2,
+    name: 'Kai Chatbot',
+    description:
+      'Kai Chatbot is a chatbot that can help you with your AI learning journey.',
+  },
+  {
+    id: 3,
+    name: 'Kai AI',
+    description:
+      'Kai AI is a collection of AI tools that can be used to help you with your AI learning journey.',
+  },
+];
+
 const HomePage = () => {
   const renderTitle = () => {
     return (
@@ -18,6 +39,28 @@ const HomePage = () => {
     );
   };
 
-  return <Grid {...styles.mainGridProps}>{renderTitle()}</Grid>;
+  const renderTools = () => {
+    return (
+      <Grid {...styles.toolsGridProps}>
+        <Grid {...styles.headerGridProps}>
+          <Typography {...styles.categoryTitleProps}>All Tools</Typography>
+        </Grid>
+        <Grid {...styles.toolsContainerGridProps}>
+          {DEFAULT_TOOLS?.map((tool) => (
+            <Grid {...styles.toolGridProps} key={tool.id}>
+              <Typography {...styles.toolTitleProps}>{tool.name}</Typography>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    );
+  };
+
+  return (
+    <Grid {...styles.mainGridProps}>
+      {renderTitle()}
+      {renderTools()}
+    </Grid>
+  );
 };
 export default HomePage;
