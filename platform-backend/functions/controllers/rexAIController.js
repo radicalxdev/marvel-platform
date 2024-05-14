0844fca3647f1253a728ea3945f95f830ee5db6f
hari-vinayak <harivinayak113@gmail.com>
2024-05-14 06:26:25 -0400

    Modfied ToolCommunicatorV2 using Busboy function

--- platform-backend/functions/controllers/rexAIController.js
+++ platform-backend/functions/controllers/rexAIController.js
@@ -6,201 +6,10 @@ const { logger } = require('firebase-functions/v1');
 const { Timestamp } = require('firebase-admin/firestore');
 const { BOT_TYPE } = require('../constants');
 const express = require('express');
-const multer = require('multer');
-const bodyParser = require('body-parser');
+const busboy = require('busboy');
 const app = express();
 
 const DEBUG = process.env.DEBUG;
-
-/**
- * Executes an axios post call to the ReX AI endpoint.
- * This communicator is currently being deprecated.
- * It is still being used for the Explain My Answer feature since no chat history is required.
- *
- * @param {object} props - The properties of the communication.
- * @param {array} props.messages - The messages to send to the server.
- * @param {string} props.user - The user making the communication.
- * @param {string} props.challengeId - The challenge id in progress.
- * @param {string} props.level - The current challenge level in progress.
- * @param {string} props.botType - The bot type involved in the communication.
- * @return {object} The response from the server.
- */
-const communicator = onCall(async (props) => {
-  try {
-    DEBUG &&
-      logger.log(
-        'Communicator variables:',
-        `API_KEY: ${process.env.REX_API_KEY_V2}`,
-        `ENDPOINT: ${process.env.REX_ENDPOINT_V2}`
-      );
-    DEBUG && logger.log('Communicator started, data:', props.data);
-
-    const { messages, user, challengeId, level, botType } = props.data;
-    const headers = {
-      'API-Key': process.env.REX_API_KEY_V2,
-      'Content-Type': 'application/json',
-    };
-
-    const resp = await axios.post(
-      process.env.REX_ENDPOINT_V2,
-      JSON.stringify({ messages, user, challengeId, level, botType }),
-      { headers }
-    );
-
-    DEBUG && logger.log('Communicator response:', resp.data);
-
-    return { status: 'success', data: resp.data };
-  } catch (error) {
-    logger.log('Communicator error:', error);
-    throw new HttpsError('internal', error.message);
-  }
-});
-
-/**
- * Executes an axios post call to the Vertex AI endpoint.
- *
- * @param {object} props - The properties of the communication.
- * @param {array} props.messages - The messages to send to the server.
- * @param {string} props.user - The user making the communication.
- * @param {string} props.challenge - The challenge in progress.
- * @param {string} props.bot - The bot involved in the communication.
- * @return {object} The response from the server.
- */
-const reXCommunicator = async (props) => {
-  try {
-    DEBUG && logger.log('Communicator started, data:', props.data);
-    const { messages, user, challengeId, level, botType } = props.data;
-
-    DEBUG &&
-      logger.log(
-        'Communicator variables:',
-        `API_KEY: ${process.env.REX_API_KEY_V2}`,
-        `ENDPOINT: ${process.env.REX_ENDPOINT_V2}`
-      );
-
-    const headers = {
-      'API-Key': process.env.REX_API_KEY_V2,
-      'Content-Type': 'application/json',
-    };
-
-    DEBUG &&
-      logger.log(
-        'Stringified JSON',
-        JSON.stringify({ messages, user, challengeId, level, botType })
-      );
-
-    const resp = await axios.post(
-      process.env.REX_ENDPOINT_V2,
-      JSON.stringify({ messages, user, challengeId, level, botType }),
-      { headers }
-    );
-
-    DEBUG && logger.log('Communicator response:', resp.data);
-
-    return { status: 'success', data: resp.data };
-  } catch (error) {
-    DEBUG && logger.log(error);
-    throw new HttpsError('internal', error.message);
-  }
-};
-
-/**
- * Executes the communicatorV2 function.
- * This new communicator is used for the mission & hackathon features.
- * It stores the messages sent & received in a chat session.
- *
- * @param {object} props - The properties passed to the function.
- * @param {object} props.data - The data object containing the message and id.
- * @param {object} props.data.message - The message object.
- * @param {string} props.data.id - The id of the chat session.
- * @return {object} The response object containing the status and data.
- * @throws {HttpsError} Throws an error if there is an internal error.
- */
-const communicatorV2 = onCall(async (props) => {
-  try {
-    DEBUG && logger.log('Communicator started, data:', props.data);
-
-    DEBUG &&
-      logger.log(
-        'Communicator variables:',
-        process.env.REX_API_KEY_V2,
-        process.env.REX_ENDPOINT_V2
-      );
-
-    const { message, id } = props.data;
-
-    DEBUG && logger.log('Message: ', message);
-    DEBUG && logger.log('ID:', id);
-
-    const chatSession = await admin
-      .firestore()
-      .collection('chatSessions')
-      .doc(id)
-      .get();
-
-    if (!chatSession.exists) {
-      logger.log('Chat session not found: ', id);
-      throw new HttpsError('not-found', 'Chat session not found');
-    }
-
-    const { user, challengeId, botType, level, messages } = chatSession.data();
-
-    DEBUG && logger.log('Chat session: ', chatSession.data());
-
-    let truncatedMessages = messages;
-
-    // Check if messages length exceeds 50, if so, truncate
-    if (messages.length > 100) {
-      truncatedMessages = messages.slice(messages.length - 65);
-    }
-
-    // Add message to chat session
-    const updatedMessages = truncatedMessages.concat([
-      { ...message, timestamp: Timestamp.fromMillis(Date.now()) },
-    ]);
-    await chatSession.ref.update({ messages: updatedMessages });
-
-    const ReXPayload = {
-      messages: updatedMessages,
-      user,
-      challengeId,
-      level,
-      botType,
-    };
-
-    DEBUG && logger.log('ReXPayload: ', ReXPayload);
-    // Communicate to ReX AI
-    const response = await reXCommunicator({
-      data: ReXPayload,
-    });
-
-    DEBUG && logger.log('ReX Response: ', response, 'type', typeof response);
-
-    // Add response to chat session
-    const updatedResponseMessages = updatedMessages.concat(
-      Array.isArray(response.data?.messages)
-        ? response.data?.messages.map((message) => ({
-            ...message,
-            timestamp: Timestamp.fromMillis(Date.now()),
-          }))
-        : [{ ...response.data, timestamp: Timestamp.fromMillis(Date.now()) }]
-    );
-    await chatSession.ref.update({ messages: updatedResponseMessages });
-
-    if (DEBUG) {
-      logger.log(
-        'Updated chat session: ',
-        (await chatSession.ref.get())?.data()
-      );
-    }
-    logger.log('Successfully communicated');
-
-    return { status: 'success' };
-  } catch (error) {
-    DEBUG && logger.log(error);
-    throw new HttpsError('internal', error.message);
-  }
-});
 
 /**
  * Simulates communication with a Kai AI endpoint.
@@ -215,12 +24,11 @@ const kaiCommunicator = async (payload) => {
 
     const { messages, user, tool_data, type } = payload.data;
 
-    DEBUG &&
-      logger.log(
-        'Communicator variables:',
-        `API_KEY: ${process.env.KAI_API_KEY}`,
-        `ENDPOINT: ${process.env.KAI_ENDPOINT}`
-      );
+    console.log(
+      'Communicator variables:',
+      `API_KEY: ${process.env.KAI_API_KEY}`,
+      `ENDPOINT: ${process.env.KAI_ENDPOINT}`
+    );
 
     const headers = {
       'API-Key': process.env.KAI_API_KEY,
@@ -230,14 +38,18 @@ const kaiCommunicator = async (payload) => {
     const kaiPayload = {
       user,
       type,
-      ...(type === BOT_TYPE.CHAT ? messages : tool_data),
+      ...(type === BOT_TYPE.CHAT ? { messages } : { tool_data }),
     };
 
-    DEBUG && logger.log('Stringified JSON', JSON.stringify(kaiPayload));
+    console.log('Stringified JSON', JSON.stringify(kaiPayload));
 
-    const resp = await axios.post(ENDPOINT, JSON.stringify(kaiPayload), {
-      headers,
-    });
+    const resp = await axios.post(
+      process.env.KAI_ENDPOINT,
+      kaiPayload,
+      {
+        headers,
+      }
+    );
 
     DEBUG && logger.log('kaiCommunicator response:', resp.data);
 
@@ -456,16 +268,6 @@ const getUserChatSessions = onCall(async (props) => {
   }
 });
 
-// Configure multer for file uploads
-const multerMiddleware = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB
-}).fields([{ name: 'file', maxCount: 3 }]); // Adjust this based on your expected files
-
-// Enable JSON and URL-encoded form body parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 /**
  * Handles tool communications by processing input data and optional file uploads.
  * It supports both JSON and form-data requests to accommodate different client implementations.
@@ -475,51 +277,73 @@ app.use(bodyParser.urlencoded({ extended: true }));
  * @return {void} Sends a response to the client based on the processing results.
  * @throws {HttpsError} Throws an error if processing fails or data is invalid.
  */
-app.post('/toolCommunicatorV2', multerMiddleware, async (req, res) => {
-  try {
-    const DEBUG = true;
-    DEBUG && logger.log('toolCommunicatorV2 started, request data:', req.body);
+app.post('/', (req, res) => {
+  const bb = busboy({ headers: req.headers });
+  const files = [];
+  const data = {};
 
-    // Deconstruct request body and files
-    const { user, type, tool_data } = req.body;
-    const files = req.files;
+  // Handle file uploads
+  bb.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    let fileBuffer = Buffer.from('');
+    file.on('data', (data) => {
+      fileBuffer = Buffer.concat([fileBuffer, data]);
+    });
+    file.on('end', () => {
+      if (filename) {
+        // This check ensures that a file was actually uploaded
+        files.push({
+          buffer: fileBuffer,
+          originalname: filename,
+          mimetype: mimetype,
+        });
+      }
+    });
+  });
 
-    // Deconstruct tool_data
-    const { tool_id, inputs } = tool_data;
+  // Handle text fields
+  bb.on('field', (fieldname, val) => {
+    data[fieldname] = val;
+  });
 
-    // Prepare payload for kaiCommunicator
-    const payload = {
-      data: {
-        user,
-        type: type,
-        tool_data: {
-          tool_id: tool_id, // Ensure tool_id is included
-          inputs: inputs,
-          file: files.map(file => ({
-            buffer: file.buffer,
-            originalname: file.originalname,
-            mimetype: file.mimetype,
-          })),
+  // Finish processing and respond
+  bb.on('finish', async () => {
+    try {
+      console.log('toolCommunicatorV2 started, request data:', data);
+      const { user, type, tool_data } = data;
+      const tool_data_parsed = JSON.parse(tool_data);
+
+      const payload = {
+        data: {
+          user,
+          type,
+          tool_data: {
+            tool_id: tool_data_parsed.tool_id,
+            inputs: tool_data_parsed.inputs,
+            files: files
+          },
         },
-      },
-    };
+      };
 
-    DEBUG && logger.log('Prepared payload for kaiCommunicator:', payload);
+      console.log('Prepared payload for kaiCommunicator:', payload);
+      DEBUG && logger.log('files', files);
 
-    // Call kaiCommunicator with constructed payload
-    const response = await kaiCommunicator(payload);
+      // Call kaiCommunicator with constructed payload
+      //const response = { data: 'dummy' };
 
-    // Process response from AI API
-    DEBUG && logger.log('Response from AI API:', response);
+      const response = await kaiCommunicator(payload);
 
-    res.status(200).send({ status: 'success', data: response.data });
-  } catch (error) {
-    const DEBUG = true;
-    DEBUG && logger.error('toolCommunicatorV2 error:', error);
-    res
-      .status(500)
-      .send({ error: 'Internal Server Error', message: error.message });
-  }
+      console.log('Response from AI API:', response);
+
+      res.status(200).send({ status: 'success', data: response.data });
+    } catch (error) {
+      console.error('toolCommunicatorV2 error:', error);
+      res
+        .status(500)
+        .send({ error: 'Internal Server Error', message: error.message });
+    }
+  });
+
+  bb.end(req.rawBody);
 });
 
 /**
@@ -632,8 +456,6 @@ const createChatSession = onCall(async (props) => {
 });
 
 module.exports = {
-  communicator,
-  communicatorV2,
   communicatorV3,
   toolCommunicatorV1,
   toolCommunicatorV2: functions.https.onRequest(app),
