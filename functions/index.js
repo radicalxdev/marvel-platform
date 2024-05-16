0c0aebd97f0062620612e7d1f418e9fdae484e20
Yunus J <contact@yunusj.com>
2024-05-17 00:15:25 +0500

    moved firebase config to root for easier edit and extra removed google service account dependency

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,4 @@
 const admin = require('firebase-admin');
-const SERVICE_FILE = require('./../google-service-account.json');
-
-const GOOGLE_APPLICATION_CREDENTIALS = SERVICE_FILE;
 const FIREBASE_CONFIG = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
