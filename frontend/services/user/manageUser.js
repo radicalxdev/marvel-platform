a391d5be0c6db0cb1ee6ce170ab3b210b50858fc
Billy K. Bett <82142770+bkb-Git@users.noreply.github.com>
2024-05-17 19:13:44 +0300

    fixed continue url issue

--- frontend/services/user/manageUser.js
+++ frontend/services/user/manageUser.js
@@ -2,14 +2,14 @@ import { sendEmailVerification, sendPasswordResetEmail } from 'firebase/auth';
 
 const sendVerification = (user) => {
   return sendEmailVerification(user, {
-    url: `/`,
+    url: window.location.origin,
     handleCodeInApp: true,
   });
 };
 
 const sendPasswordReset = (auth, email) => {
   return sendPasswordResetEmail(auth, email, {
-    url: `/signin`,
+    url: `${window.location.origin}/signin`,
     handleCodeInApp: true,
   });
 };
