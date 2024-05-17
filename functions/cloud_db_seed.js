1038c9b359b3bafd7f627c146fb431bd9870cfe9
Billy K. Bett <82142770+bkb-Git@users.noreply.github.com>
2024-05-18 00:24:31 +0300

    refactored seedDatabase function

--- functions/cloud_db_seed.js
+++ functions/cloud_db_seed.js
@@ -1,4 +1,5 @@
 const admin = require('firebase-admin');
+const { Timestamp } = require('firebase-admin/firestore');
 
 const db = admin.firestore();
 
@@ -6,13 +7,24 @@ const seedDatabase = async () => {
   const data = require('./seed_data.json');
 
   try {
+    const global = await db.collection('global').doc('config').get();
+
+    if (global.exists) {
+      console.log('Kai AI is ready to go!');
+      return;
+    }
+
+    db.collection('global')
+      .doc('config')
+      .set({ dbSeeded: true, updatedAt: Timestamp.fromMillis(Date.now()) });
+
     Object.values(data).forEach(async (doc) => {
-      db.collection('markers')
-        .doc('seedingDone')
-        .set({ timestamp: new Date() });
       await db.collection('tools').doc(doc.id.toString()).set(doc);
       console.log(`Document with ID ${doc.id} added to the Tools collection`);
     });
+    console.log(
+      'Kai AI installed successfully to firebase and is ready to go!'
+    );
   } catch (error) {
     console.error('Error seeding database:', error);
   }
