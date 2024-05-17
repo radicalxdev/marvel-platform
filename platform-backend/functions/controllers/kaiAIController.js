c8cdc5ab484e5a0020e35e3962d81b5c9b739fea
Billy K. Bett <82142770+bkb-Git@users.noreply.github.com>
2024-05-17 03:23:24 +0300

    changed controller names for kai

--- platform-backend/functions/controllers/kaiAIController.js
+++ platform-backend/functions/controllers/kaiAIController.js
@@ -81,7 +81,7 @@ const kaiCommunicator = async (payload) => {
  *
  * @return {object} The response object containing the status and data.
  */
-const communicator = onCall(async (props) => {
+const chat = onCall(async (props) => {
   try {
     DEBUG && logger.log('Communicator started, data:', props.data);
 
@@ -351,7 +351,7 @@ const createChatSession = onCall(async (props) => {
 });
 
 module.exports = {
-  communicator,
-  toolCommunicator: functions.https.onRequest(app),
+  chat,
+  tool: functions.https.onRequest(app),
   createChatSession,
 };
