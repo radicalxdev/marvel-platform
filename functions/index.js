7464b760df2a713c8f206f5cc8284f828adf9d6f
Billy K. Bett <82142770+bkb-Git@users.noreply.github.com>
2024-05-20 10:01:06 +0300

    delete: Delete dbSeedController
    	- removed logic relating to dbSeedController
    	- changed console logs on kaiCommunicator to logger logs

--- functions/index.js
+++ functions/index.js
@@ -6,13 +6,11 @@ admin.initializeApp();
 const userController = require('./controllers/userController');
 const kaiAIController = require('./controllers/kaiAIController');
 const { seedDatabase } = require('./cloud_db_seed');
-// const dbSeedController = require('./controllers/dbSeedController');
 
 seedDatabase();
 
 /* Migration Scripts */
 // const {
-//   modifyChallengePlayersData,
 // } = require("./migrationScripts/modifyChallengePlayersData");
 const migrationScripts = {};
 
@@ -24,8 +22,6 @@ module.exports = {
   chat: kaiAIController.chat,
   tool: kaiAIController.tool,
   createChatSession: kaiAIController.createChatSession,
-
-  // seedDatabase: dbSeedController.seedDatabase,
 
   /* Migration Scripts - For running  */
   ...migrationScripts,
