834dbdfb61df5aa55473a70a97e35d30080b6a0b
BruceBest <BruceLvJC@outlook.com>
2024-05-30 12:59:41 -0400

    make second version getHist
    make getHist dupport both GET and POST request on 2 different function, easy to swap between

--- functions/controllers/userController.js
+++ functions/controllers/userController.js
@@ -36,7 +36,7 @@ exports.signUpUser = https.onCall(async (data, context) => {
  * @throws {HttpsError} If any of the required fields (email, fullName, uid) are missing in the data object.
  * @return {Object} status and the user's history in a list form
  */
-exports.getHist = https.onCall(async (data) => {
+const histPOST = https.onCall(async (data) => {
   const { uid } = data;
   if (!uid) {
     throw new https.HttpsError(
@@ -52,3 +52,35 @@ exports.getHist = https.onCall(async (data) => {
     .get();
   return { status: 'success', data: hists.docs.map((doc) => doc.data()) };
 });
+
+/**
+ * Creates a new user document in the Firestore collection "users" with the provided data.
+ *
+ * @param {Object} data - The data object containing the user's unique identifier (uid).
+ * @throws {HttpsError} If any of the required fields (email, fullName, uid) are missing in the data object.
+ */
+const histGET = https.onRequest(async (req, res) => {
+  if (req.method === 'GET') {
+    const { uid } = req.body.data;
+    if (!uid) {
+      throw new https.HttpsError(
+        'failed-precondition',
+        'Please provide all required fields'
+      );
+    }
+    const hists = await admin
+      .firestore()
+      .collection('users')
+      .doc(uid)
+      .collection('Hists')
+      .get();
+    res
+      .status(200)
+      .send({ status: 'success', data: hists.docs.map((doc) => doc.data()) });
+  } else {
+    // Handle other types of requests
+    res.status(405).send('Method Not Allowed');
+  }
+});
+
+exports.getHist = histGET; // or histPOST if want use post to access
