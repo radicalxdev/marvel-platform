a62c8bec0e216fd6075ff0af503ed2605cd75067
BruceBest <BruceLvJC@outlook.com>
2024-06-03 13:46:10 -0400

    hash util created

---
+++ functions/utils/hashUtil.js
@@ -0,0 +1,13 @@
+exports.stringToHash = (string) => {
+  let hash = 0;
+
+  if (string.length == 0) return hash;
+
+  for (let i = 0; i < string.length; i++) {
+    const char = string.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash = hash & hash;
+  }
+
+  return hash;
+};
