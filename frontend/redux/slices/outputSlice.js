56a3569d225a0312610f12c951762eeadb3a55f8
BruceBest <BruceLvJC@outlook.com>
2024-06-11 15:14:31 -0400

    finished output slice with it's thrunk
    review needed

---
+++ frontend/redux/slices/outputSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit'
+import fetchOutput from '../thunks/output'
+
+const initialState = {
+  loading: true,
+  outputs: [],
+  error: null,
+}
+
+const output = createSlice({
+  name: 'output',
+  initialState,
+  reducers: {
+    reset: () => initialState,
+    add: (state, action) => {
+      state.outputs = state.outputs.concat(action.payload)
+    },
+    update: (state, action) => {
+      let newOut = []
+      state.outputs.forEach((i) => {
+        if (i.id === action.payload.id) {
+          newOut = newOut.concat(action.payload)
+        } else {
+          newOut = newOut.concat(i)
+        }
+      })
+      state.outputs = newOut
+    },
+    del: (state, action) => {
+      let newOut = []
+      state.outputs.forEach((i) => {
+        if (i.id !== action.payload.id) {
+          newOut = newOut.concat(i)
+        }
+      })
+      state.outputs = newOut
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchOutput.pending, (state) => {
+          state.loading = true
+        })
+        .addCase(fetchOutput.fulfilled, (state, action) => {
+          state.output = action.payload
+          state.loading = false
+        })
+        .addCase(fetchOutput.rejected, (state) => {
+          state.error = 'Could not get tools'
+          state.loading = false
+        })
+    },
+  },
+})
+
+export const { reset, add, update, del } = output.actions
+
+export default output.reducer
---
+++ frontend/redux/thunks/output.js
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import 'firebase/firestore'
+
+const fetchOutput = createAsyncThunk(
+  'tools/fetch',
+  async ({ firestore, uid }) => {
+    try {
+      const hists = await firestore
+        .firestore()
+        .collection('users')
+        .doc(uid)
+        .collection('outputs')
+        .get()
+      return hists.docs.map((doc) => doc.data())
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+)
+
+export default fetchOutput
