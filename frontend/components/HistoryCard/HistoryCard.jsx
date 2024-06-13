e2a2e38e449764ef26586626057340eeae3bcf05
LiangChu1 <chuliang686@gmail.com>
2024-06-13 16:39:51 -0400

    started working on the individual card component for the output history (need to fix css)

---
+++ frontend/components/HistoryCard/HistoryCard.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+import { Button, Card, Grid, Typography } from '@mui/material';
+
+import Image from 'next/image';
+
+import styles from './styles';
+
+const HistoryCard = (props) => {
+  const { backgroundImgURL, title, logo, description, createdDate } = props;
+  const [openSubScreen, setOpenSubScreen] = useState(false);
+
+  const toggleSubScreen = () => {
+    setOpenSubScreen(!openSubScreen);
+  };
+
+  const renderImage = () => {
+    return (
+      <Grid {...styles.imageGridProps}>
+        <Image src={logo} alt='output history logo' {...styles.imageProps} />
+      </Grid>
+    );
+  };
+
+  const renderCardDetails = () => {
+    return (
+      <Grid {...styles.contentGridProps}>
+        <Typography {...styles.dateProps}>{createdDate}</Typography>
+        <Typography {...styles.titleProps}>{title}</Typography>
+        <Typography {...styles.descriptionProps}>{description}</Typography>
+        <Button {...styles.previewButtonProps} onClick={toggleSubScreen}>
+          Preview
+        </Button>
+      </Grid>
+    );
+  };
+
+  return (
+    <Grid {...styles.mainGridProps}>
+      <Grid {...styles.historycardProps}>
+        <Card {...styles.cardProps(backgroundImgURL)}>{renderImage()}</Card>
+        <Grid {...styles.toolDetailsGridProps}>{renderCardDetails()}</Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default HistoryCard;
---
+++ frontend/components/HistoryCard/styles.js
@@ -0,0 +1,95 @@
+import { getRandomBackgroundColor } from '@/utils/MiscellaneousUtils';
+//Styling needs fixing here
+const styles = {
+  mainGridProps: {
+    container: true,
+    item: true,
+  },
+  historycardProps: {
+    container: true,
+    item: true,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  cardProps: (backgroundImgURL) => ({
+    elevation: 5,
+    sx: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+      position: 'relative',
+      height: '200px',
+      width: '100%',
+      borderRadius: '10px',
+      overflow: 'hidden',
+      p: 2,
+      ...(backgroundImgURL && {
+        backgroundImage: `url(${backgroundImgURL})`,
+        backgroundSize: 'cover',
+      }),
+      ...(!backgroundImgURL && {
+        background: getRandomBackgroundColor(),
+      }),
+      transition: (theme) => theme.transitions.create('all'),
+      '&:hover': {
+        cursor: 'pointer',
+        transform: 'scale(1.05)',
+      },
+    },
+  }),
+  imageGridProps: {
+    position: 'relative',
+    container: true,
+    item: true,
+    width: 48,
+    height: 48,
+    borderRadius: '50%',
+  },
+  imageProps: {
+    layout: 'fill',
+    objectFit: 'cover',
+  },
+  contentGridProps: {
+    container: true,
+    item: true,
+    direction: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  dateProps: {
+    fontFamily: 'Satoshi Regular',
+    fontSize: '12px',
+    color: (theme) => theme.palette.Common.Black['100p'],
+  },
+  titleProps: {
+    fontFamily: 'Satoshi Bold',
+    fontSize: '16px',
+    color: (theme) => theme.palette.Common.Black['100p'],
+  },
+  descriptionProps: {
+    fontFamily: 'Satoshi Regular',
+    fontSize: '14px',
+    color: (theme) => theme.palette.Common.Black['100p'],
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    sx: {
+      display: '-webkit-box',
+      WebkitLineClamp: 1,
+      WebkitBoxOrient: 'vertical',
+    },
+  },
+  previewButtonProps: {
+    variant: 'contained',
+    color: 'primary',
+    size: 'small',
+  },
+  toolDetailsGridProps: {
+    container: true,
+    item: true,
+    direction: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+};
+
+export default styles;
