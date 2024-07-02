2aaafd773dd3504dc23e39e614025ca35446d6dc
Reza <homayunir@gmail.com>
2024-07-02 12:15:36 -0400

    removed unnecessary reducer actions in slices

--- frontend/redux/slices/toolsHistorySlice.js
+++ frontend/redux/slices/toolsHistorySlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import fetchToolsHistory from '../thunks/toolsHistory';
 
 const initialState = {
-  loading: true,
+  loading: false,
   data: [],
   error: null,
 };
@@ -12,21 +12,12 @@ const toolsHistorySlice = createSlice({
   name: 'toolsHistory',
   initialState,
   reducers: {
-    resetToolsHistory: (state) => {
-      state.loading = true;
-      state.data = [];
-      state.error = null;
-      localStorage.removeItem('toolsHistory');
-    },
-    setToolsHistoryLoading: (state, action) => {
-      state.loading = action.payload;
-      state.error = null; // Clear any existing error when loading state changes
-    },
+    // add reducer functions here if needed
   },
   extraReducers: (builder) => {
     builder
       .addCase(fetchToolsHistory.pending, (state) => {
-        state.loading = true;
+        state.loading = false;
         state.error = null;
       })
       .addCase(fetchToolsHistory.fulfilled, (state, action) => {
@@ -41,7 +32,6 @@ const toolsHistorySlice = createSlice({
   },
 });
 
-export const { resetToolsHistory, setToolsHistoryLoading } =
-  toolsHistorySlice.actions;
+export const { resetToolsHistory } = toolsHistorySlice.actions;
 
 export default toolsHistorySlice.reducer;
