06b8bc09047af9777f4315976bcf321b354188f8
Michael Ilerioluwa Adeniyi <77995341+mickey4653@users.noreply.github.com>
2024-07-04 15:19:50 -0400

    Made some Changes: 	- added more quick action buttons

--- frontend/templates/Chat/QuickActionButton/QuickActionButton.jsx
+++ frontend/templates/Chat/QuickActionButton/QuickActionButton.jsx
@@ -1,8 +1,20 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
-import { Button, CircularProgress, Grid, IconButton } from '@mui/material';
+import {
+  Button,
+  CircularProgress,
+  ClickAwayListener,
+  Grid,
+  IconButton,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+} from '@mui/material';
 
 import { useDispatch } from 'react-redux';
+
+import style from 'react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark';
 
 import ActionIcon from '@/assets/svg/add-circle.svg';
 
@@ -15,13 +27,27 @@ const QuickActionButton = (props) => {
 
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
 
   const handleQuickAction = useCallback(() => {
     setLoading(true);
-    dispatch(setInput(onAction));
+
     // Add any other logic you want to perform when the button is clicked
-    setLoading(false);
+    setTimeout(() => {
+      setLoading(false);
+      setOpen((preOpen) => !preOpen);
+    }, 500);
   }, [dispatch, onAction]);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleActionClick = () => {
+    dispatch(setInput(onAction));
+    handleClose();
+  };
 
   return (
     <Grid {...styles.actionButtonGridProps}>
@@ -30,11 +56,45 @@ const QuickActionButton = (props) => {
           onClick={handleQuickAction}
           {...styles.actionButtonProps}
           disabled={loading}
+          ref={anchorRef}
         >
           {loading ? <CircularProgress size={24} /> : <ActionIcon />}
           {loading ? 'Loading...' : text}
         </Button>
       </IconButton>
+
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        placement="top"
+        disablePortal
+        {...styles.popperProps}
+      >
+        <Paper elevation={3} {...styles.paperProps}>
+          <ClickAwayListener onClickAway={handleClose}>
+            <MenuList {...styles.menuListProps}>
+              <MenuItem
+                onClick={() => handleActionClick('suggest_techniques')}
+                {...styles.menuItemProps}
+              >
+                Suggest Techniques
+              </MenuItem>
+              <MenuItem
+                onClick={() => handleActionClick('recommend_books')}
+                {...styles.menuItemProps}
+              >
+                Recommend Books
+              </MenuItem>
+              <MenuItem
+                onClick={() => handleActionClick('Action 3')}
+                {...styles.menuItemProps}
+              >
+                Action 3
+              </MenuItem>
+            </MenuList>
+          </ClickAwayListener>
+        </Paper>
+      </Popper>
     </Grid>
   );
 };
--- frontend/templates/Chat/QuickActionButton/styles.js
+++ frontend/templates/Chat/QuickActionButton/styles.js
@@ -24,6 +24,41 @@ const styles = {
       mr: { laptop: -2, desktop: -1, desktopMedium: -3 },
     }),
   },
+
+  menuListProps: {
+    sx: {
+      display: 'flex',
+      flexDirection: 'row',
+      padding: 1,
+      margin: '15px',
+    },
+  },
+  popperProps: {
+    sx: {
+      ml: { laptop: -3, desktop: -1, desktopMedium: -3 },
+    },
+  },
+  paperProps: {
+    sx: {
+      backgroundColor: 'transparent !important',
+    },
+  },
+  menuItemProps: {
+    sx: (theme) => ({
+      borderRadius: '18px',
+      margin: '0 5px',
+      borderColor: theme.palette.Background.purple3,
+      background: theme.palette.Background.purple3,
+      color: theme.palette.Common.White['100p'],
+      textTransform: 'none',
+      ':hover': {
+        backgroundColor: '#B791FF',
+        borderColor: theme.palette.Background.purple,
+        color: theme.palette.Common.White['100p'],
+      },
+      padding: '10px 20px',
+    }),
+  },
 };
 
 export default styles;
