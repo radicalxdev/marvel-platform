cdbe33dd1d61eed6be874717057ab49adb2fd8ed
Reza <homayunir@gmail.com>
2024-07-16 11:55:53 -0400

    JDocs added for HistoryToolListingContainer

--- frontend/components/HistoryToolListingContainer/HistoryToolListingContainer.jsx
+++ frontend/components/HistoryToolListingContainer/HistoryToolListingContainer.jsx
@@ -11,6 +11,15 @@ const DEFAULT_HISTORY = new Array(4)
   .fill()
   .map((_, index) => ({ id: index + 1 }));
 
+/**
+ * Renders the HistoryToolListingContainer component, which displays a list of tool cards or skeleton loaders based on loading state.
+ *
+ * @param {Object} props - The properties object.
+ * @param {Array} props.data - The array of tool data to display.
+ * @param {boolean} props.loading - The loading state indicating whether data is being fetched.
+ * @param {string} props.category - The category name to display as the title.
+ * @returns {JSX.Element} The rendered HistoryToolListingContainer component.
+ */
 const HistoryToolListingContainer = (props) => {
   const { data, loading, category } = props;
 
