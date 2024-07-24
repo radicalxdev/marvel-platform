02b04ef4316fe8e21537fa785306f50ca150c030
Reza <homayunir@gmail.com>
2024-07-24 15:15:12 -0400

    resolved toolId & onboarding dynamic routing conflicts

--- frontend/pages/onboarding/[onboardingId]/index.jsx
+++ frontend/pages/onboarding/[onboardingId]/index.jsx
@@ -1,22 +1,19 @@
 import { useRouter } from 'next/router';
 
-import OnboardingLayout from '@/layouts/OnboardingLayout/';
+import OnboardingLayout from '@/layouts/OnboardingLayout';
 import OnboardingPage from '@/templates/Onboarding';
-
-import ROUTES from '@/constants/routes';
 
 const IndividualOnboardingPage = () => {
   const router = useRouter();
-
   const { onboardingId } = router.query;
 
   if (!onboardingId) {
-    router.push(ROUTES.HOME);
     return null;
   }
 
   return <OnboardingPage />;
 };
+
 IndividualOnboardingPage.getLayout = function getLayout(page) {
   return <OnboardingLayout>{page}</OnboardingLayout>;
 };
