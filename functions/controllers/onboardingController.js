f57a43eae40c3691e665d16ba7bdb15bcad76f08
Paul Lam <paullamsj12@gmail.com>
2024-08-16 14:07:10 +0100

    feat: Add setupUserProfile cloud function to update user profile in Firestore
    - Implemented `setupUserProfile` as an HTTPS callable Cloud Function.
    - The function updates user profile information (excluding profile picture) in Firestore.
    - Added validation for required fields: `uid`, `fullName`, `occupation`, `socialLinks`, and `bio`.
    - Ensured `fullName` is validated using a regex pattern.
    - Logs updates and handles errors with appropriate HTTP status codes.
    - Provided a detailed JSDoc comment for usage and function documentation.

--- functions/controllers/onboardingController.js
+++ functions/controllers/onboardingController.js
@@ -1,14 +1,17 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const {logger} = functions;
+/* eslint-disable require-jsdoc */
+/* eslint-disable valid-jsdoc */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const { logger } = functions;
 const DEBUG = process.env.DEBUG;
 
 async function progressOnboardingStatus(id, progressOnly = false) {
   DEBUG && logger.log('Advance Onboarding Status method started');
 
-  //Validate if userId is not empty string or null
+  // Validate if userId is not empty string or null
   if (!id || typeof id !== 'string') {
     DEBUG && logger.error('Invalid user ID:', id);
+    // eslint-disable-next-line no-throw-literal
     throw { code: 400, message: 'Invalid user ID' };
   }
 
@@ -20,7 +23,12 @@ async function progressOnboardingStatus(id, progressOnly = false) {
 
     // Validate if user exists
     if (!userDoc.exists) {
-      DEBUG && logger.error('Error in progressing Onboarding Status:', 'User not found');
+      DEBUG &&
+        logger.error(
+          'Error in progressing Onboarding Status:',
+          'User not found'
+        );
+      // eslint-disable-next-line no-throw-literal
       throw { code: 403, message: 'User not found' };
     }
     DEBUG && logger.info('User found in database');
@@ -31,95 +39,105 @@ async function progressOnboardingStatus(id, progressOnly = false) {
     let onboarding = user.onboarding || {};
 
     // Destructure the onboarding object with default values
-    let { 
-        1: step1 = false, 
-        2: step2 = false, 
-        3: step3 = false, 
-        4: step4 = false, 
-        5: step5 = false 
+    let {
+      1: step1 = false,
+      2: step2 = false,
+      3: step3 = false,
+      4: step4 = false,
+      5: step5 = false,
     } = onboarding;
 
     // Checking error on onboarding status structure
-    // Check if any step is missing 
+    // Check if any step is missing
     const needsUpdate = !(
-        1 in onboarding && 
-        2 in onboarding && 
-        3 in onboarding && 
-        4 in onboarding && 
-        5 in onboarding
+      1 in onboarding &&
+      2 in onboarding &&
+      3 in onboarding &&
+      4 in onboarding &&
+      5 in onboarding
     );
 
     // Check if there's an error in the sequence
     // eg. 1= false, 2 = true ...
-    const hasError = (!step1 && (step2 || step3 || step4 || step5)) ||
-        (!step2 && (step3 || step4 || step5)) || 
-        (!step3 && (step4 || step5)) || 
-        (!step4 && step5);
-    
+    const hasError =
+      (!step1 && (step2 || step3 || step4 || step5)) ||
+      (!step2 && (step3 || step4 || step5)) ||
+      (!step3 && (step4 || step5)) ||
+      (!step4 && step5);
+
     // Validate if there's an error or needs update
     if (needsUpdate || hasError) {
-        DEBUG && logger.info('Onboarding status will be updated');
+      DEBUG && logger.info('Onboarding status will be updated');
+      onboarding = {
+        1: step1,
+        2: step2,
+        3: step3,
+        4: step4,
+        5: step5,
+      };
+
+      // if there's an error, reset the onboarding status
+      if (hasError) {
         onboarding = {
-            1: step1, 
-            2: step2, 
-            3: step3, 
-            4: step4, 
-            5: step5 
+          1: false,
+          2: false,
+          3: false,
+          4: false,
+          5: false,
         };
 
-        //if there's an error, reset the onboarding status
-        if (hasError) {
-            onboarding = { 
-                1: false, 
-                2: false, 
-                3: false, 
-                4: false, 
-                5: false 
-            };
-
-            // Reset the steps variables I am using as reference to my onboarding status
-            step1 = false;
-            step2 = false;
-            step3 = false;
-            step4 = false;
-            step5 = false;
-        }
-        // Update the user's onboarding status
-        await userRef.update({ onboarding });
-        DEBUG && logger.info('Onboarding status updated successfully');
+        // Reset the steps variables I am using as reference to my onboarding status
+        step1 = false;
+        step2 = false;
+        step3 = false;
+        step4 = false;
+        step5 = false;
+      }
+      // Update the user's onboarding status
+      await userRef.update({ onboarding });
+      DEBUG && logger.info('Onboarding status updated successfully');
     }
 
     // Get the current status (first false step)
     // If all steps are false, return 1
     // If all steps are true, return 6
     // 6 means that onboarding is completed
-    const currentOnboardingStatus = 
-    !step1 ? 1 : 
-    !step2 ? 2 : 
-    !step3 ? 3 : 
-    !step4 ? 4 : 
-    !step5 ? 5 : 6;
+    const currentOnboardingStatus = !step1
+      ? 1
+      : !step2
+        ? 2
+        : !step3
+          ? 3
+          : !step4
+            ? 4
+            : !step5
+              ? 5
+              : 6;
 
     DEBUG && logger.info('Current Onboarding Status:', currentOnboardingStatus);
 
     // Verify if onboaring is completed
     if (currentOnboardingStatus >= 6) {
-        DEBUG && logger.info('Onboarding is completed');
-        throw { code: 403, message: 'Onboarding is completed already!' };
+      DEBUG && logger.info('Onboarding is completed');
+      // eslint-disable-next-line no-throw-literal
+      throw { code: 403, message: 'Onboarding is completed already!' };
     }
 
     // Do not progress if progressOnly and
     // current onboarding status is 2 or 3
-    if (progressOnly && 
-        (currentOnboardingStatus === 2 ||
-        currentOnboardingStatus === 3)) {
-            DEBUG && logger.info('Progress by other function on onboarding status 2 or 3');
-            throw {
-                success: false, 
-                code: 403, 
-                message: 'Save form first to progress onboaring status',
-                currentOnboardingStatus
-        };
+    if (
+      progressOnly &&
+      (currentOnboardingStatus === 2 || currentOnboardingStatus === 3)
+    ) {
+      DEBUG &&
+        logger.info('Progress by other function on onboarding status 2 or 3');
+      // eslint-disable-next-line no-throw-literal
+      throw {
+        success: false,
+        code: 403,
+        message: 'Save form first to progress onboaring status',
+        currentOnboardingStatus,
+      };
     }
 
     // Update the current status to true
@@ -130,10 +148,9 @@ async function progressOnboardingStatus(id, progressOnly = false) {
     DEBUG && logger.info('Onboarding status progressed successfully');
 
     return {
-        success: true,
-        message: 'Onboarding status progressed'
-      };
-
+      success: true,
+      message: 'Onboarding status progressed',
+    };
   } catch (error) {
     DEBUG && logger.error('Error in progressing Onboarding Status:', error);
     throw error;
@@ -142,21 +159,21 @@ async function progressOnboardingStatus(id, progressOnly = false) {
 
 /**
  * Advances the onboarding status of a user by updating the onboarding object in the user document.
- * 
+ *
  * @function advanceOnboardingStatus
- * 
+ *
  * @param {Object} data - The data object passed from the client.
  * @param {string} data.uid - The user ID for which to advance the onboarding status.
  * @param {functions.https.CallableContext} context - The Firebase callable context.
- * 
+ *
  * @returns {Promise<Object>} A promise that resolves to an object containing:
  *   @returns {boolean} success - Indicates whether the operation was successful.
  *   @returns {string} message - A message describing the result of the operation.
- * 
+ *
  * @throws {functions.https.HttpsError} Possible error cases:
  * @throws {string} code - The error code.
  * @throws {string} message - The error message.
- * 
+ *
  * @example
  * // Usage as an HTTP endpoint
  * // POST request to: function link
@@ -166,7 +183,7 @@ async function progressOnboardingStatus(id, progressOnly = false) {
  * //     "uid": "user-id"
  * //   }
  * // }
- * 
+ *
  * // Example using fetch API:
  * fetch('function link, {
  *   method: 'POST',
@@ -183,39 +200,127 @@ async function progressOnboardingStatus(id, progressOnly = false) {
  * .then(result => console.log(result))
  * .catch(error => console.error('Error:', error));
  */
-const advanceOnboardingStatus = functions.https.onCall(async (data, context) => {
-  // Getting the userId from data
-  const {uid} = data;
-  DEBUG && logger.log('advance Onboarding Status started, data:', uid);
+const advanceOnboardingStatus = functions.https.onCall(
+  async (data, context) => {
+    // Getting the userId from data
+    const { uid } = data;
+    DEBUG && logger.log('advance Onboarding Status started, data:', uid);
 
-  // Validate if userId is not null or not a string
-  if (!uid || typeof uid !== 'string') {
-    throw new functions.https.HttpsError('invalid-argument', 'Invalid uid');
-  }
-  try {
-    // Progress the onboarding status of the user
-    const result = await progressOnboardingStatus(uid, true);
+    // Validate if userId is not null or not a string
+    if (!uid || typeof uid !== 'string') {
+      throw new functions.https.HttpsError('invalid-argument', 'Invalid uid');
+    }
+    try {
+      // Progress the onboarding status of the user
+      const result = await progressOnboardingStatus(uid, true);
 
-    return { 
-      success : result.success,
-      message: result.message
-    };
-  } catch (error) {
-    DEBUG && logger.error('Error advancing Onboarding Status:', error);
-    if (error.code === 400) {
+      return {
+        success: result.success,
+        message: result.message,
+      };
+    } catch (error) {
+      DEBUG && logger.error('Error advancing Onboarding Status:', error);
+      if (error.code === 400) {
         throw new functions.https.HttpsError('invalid-argument', error.message);
-    }
-    if (error.code === 403) {
+      }
+      if (error.code === 403) {
         throw new functions.https.HttpsError('internal', error.message);
+      }
+      if (error.code === 404) {
+        throw new functions.https.HttpsError('internal', error.message);
+      }
+      throw new functions.https.HttpsError(
+        'internal',
+        `An unexpected error occurred: ${error.message}`
+      );
     }
-    if (error.code === 404) {
-      throw new functions.https.HttpsError('internal', error.message);
+  }
+);
+
+/**
+ * Updates user profile information in Firestore except for profile picture.
+ *
+ * This Cloud Function is triggered via an HTTPS callable request and expects
+ * user profile data to be provided in the request payload. The function validates
+ * the input data, ensures required fields are present, and updates the user's
+ * profile document in the Firestore database.
+ * It parses json/string data and therefore cannot be used to upload images to.
+ *
+ * @function setupUserProfile
+ * @async
+ *
+ * @param {Object} data - The data object containing user profile information.
+ * @param {string} data.uid - The unique identifier for the user.
+ * @param {string} data.fullName - The full name of the user.
+ * @param {string} data.occupation - The occupation of the user.
+ * @param {Object} data.socialLinks - The social media links associated with the user.
+ * @param {string} data.bio - A short biography of the user.
+ *
+ * @param {Object} context - The context object containing authentication information.
+ *
+ * @returns {Object} A success message indicating the profile was updated.
+ *
+ * @throws {functions.https.HttpsError} 'invalid-argument' If any required fields are missing or invalid.
+ * @throws {functions.https.HttpsError} 'internal' If an internal error occurs while updating the profile.
+ *
+ * @example
+ * // Example usage in frontend:
+ * const result = await firebase.functions().httpsCallable('setupUserProfile')({
+ *   uid: '123',
+ *   fullName: 'John Doe',
+ *   occupation: 'Software Engineer',
+ *   socialLinks: { twitter: 'https://twitter.com/johndoe' },
+ *   bio: 'A passionate developer.'
+ * });
+ * console.log(result.data.message); // "User profile updated successfully"
+ */
+
+const setupUserProfile = functions.https.onCall(async (data, context) => {
+  try {
+    const { uid, fullName, occupation, socialLinks, bio } = data;
+
+    // Validate fields
+    if (!uid || !fullName || !occupation || !socialLinks || !bio) {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'Missing required fields.'
+      );
     }
-    throw new functions.https.HttpsError('internal', `An unexpected error occurred: ${error.message}`);
+
+    // Validate fullName
+    const nameRegex = /^[a-zA-Z0-9. ]+$/;
+    if (!nameRegex.test(fullName)) {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'Please input a valid full name.'
+      );
+    }
+
+    // Store user data in Firestore
+    const userRef = admin.firestore().collection('users').doc(uid);
+    const userDoc = {
+      id: uid,
+      fullName: fullName,
+      occupation: occupation,
+      socialLink: socialLinks,
+      bio: bio,
+    };
+
+    logger.log('Updating user document in Firestore', userDoc);
+
+    await userRef.set(userDoc, { merge: true });
+
+    logger.log('User profile updated successfully');
+
+    return { message: 'User profile updated successfully' };
+  } catch (error) {
+    logger.error('Error updating user profile:', error);
+    throw new functions.https.HttpsError('internal', error.message);
   }
 });
 
 module.exports = {
-    advanceOnboardingStatus,
-    progressOnboardingStatus 
+  advanceOnboardingStatus,
+  progressOnboardingStatus,
+  setupUserProfile,
 };
