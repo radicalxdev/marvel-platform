70d1694b059b986c1c7698acfc66f0ccfe9f722d
billy <billykbett@gmail.com>
2024-08-19 17:08:33 +0300

    changed home welcome message

--- frontend/templates/HomePage/HomePage.jsx
+++ frontend/templates/HomePage/HomePage.jsx
@@ -12,7 +12,9 @@ const HomePage = (props) => {
       <Grid {...styles.titleGridProps}>
         <Typography {...styles.titleProps}>
           Welcome to{' '}
-          <Typography {...styles.highlightTextProps}>Marvel Tools</Typography>{' '}
+          <Typography {...styles.highlightTextProps}>
+            Marvel AI Tools
+          </Typography>{' '}
           👋
         </Typography>
         <Typography {...styles.subtitleProps}>
