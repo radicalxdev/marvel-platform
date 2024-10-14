585655e14cc4bb18204d28133fd71353f3d59808
ademiltonnunes <ademilton.marcelo1@gmail.com>
2024-10-14 15:17:53 -0700

    Updated how to slide transition screens

--- frontend/components/SlideTransition/SlideTransition.jsx
+++ frontend/components/SlideTransition/SlideTransition.jsx
@@ -1,27 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { Slide } from '@mui/material';
 
-const SlideTransition = ({ children }) => {
+const SlideTransition = ({ children, direction = 'up', duration = 300 }) => {
   const [currentChildren, setCurrentChildren] = useState(children);
   const [slideIn, setSlideIn] = useState(true);
 
+  const compareChildren = useCallback((prevChildren, nextChildren) => {
+    if (prevChildren === null || nextChildren === null) {
+      return prevChildren === nextChildren;
+    }
+    return prevChildren.type === nextChildren.type;
+  }, []);
+
   useEffect(() => {
     let timer;
-    if (children !== currentChildren) {
+    if (!compareChildren(children, currentChildren)) {
       setSlideIn(false);
       timer = setTimeout(() => {
         setCurrentChildren(children);
         setSlideIn(true);
-      }, 300);
+      }, duration);
     }
     return () => {
       if (timer) clearTimeout(timer);
     };
-  }, [children, currentChildren]);
+  }, [children, currentChildren, compareChildren, duration]);
 
   return (
-    <Slide direction="up" in={slideIn} timeout={300}>
+    <Slide direction={direction} in={slideIn} timeout={duration}>
       <div>{currentChildren}</div>
     </Slide>
   );
